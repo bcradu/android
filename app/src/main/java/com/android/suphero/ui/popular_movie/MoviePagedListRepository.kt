81ac4ca2cbbe3e1127948ba5e86beffd462c5d4b
bcradu <bcradu69@gmail.com>
2020-05-12 21:43:55 +0300

    first commit

---
+++ app/src/main/java/com/android/suphero/ui/popular_movie/MoviePagedListRepository.kt
@@ -0,0 +1,37 @@
+package com.android.suphero.ui.popular_movie
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.android.suphero.data.api.POST_PER_PAGE
+import com.android.suphero.data.api.TheMovieDBInterface
+import com.android.suphero.data.repository.MovieDataSource
+import com.android.suphero.data.repository.MovieDataSourceFactory
+import com.android.suphero.data.repository.NetworkState
+import com.android.suphero.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MoviePagedListRepository (private val apiService : TheMovieDBInterface) {
+
+    lateinit var moviePagedList: LiveData<PagedList<Movie>>
+    lateinit var moviesDataSourceFactory: MovieDataSourceFactory
+
+    fun fetchLiveMoviePagedList (compositeDisposable: CompositeDisposable) : LiveData<PagedList<Movie>> {
+        moviesDataSourceFactory = MovieDataSourceFactory(apiService, compositeDisposable)
+
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setPageSize(POST_PER_PAGE)
+            .build()
+
+        moviePagedList = LivePagedListBuilder(moviesDataSourceFactory, config).build()
+
+        return moviePagedList
+    }
+
+    fun getNetworkState(): LiveData<NetworkState> {
+        return Transformations.switchMap<MovieDataSource, NetworkState>(
+            moviesDataSourceFactory.moviesLiveDataSource, MovieDataSource::networkState)
+    }
+}
